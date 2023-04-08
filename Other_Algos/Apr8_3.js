fb55d3d4603d730dab156261216cbc37e5413b03
PeterVasilopoulos <peterjvasilopoulos@gmail.com>
2023-04-08 16:40:40 -0400

    Updated files

---
+++ Other_Algos/Apr8_3.js
@@ -0,0 +1,37 @@
+// Algo Description: 
+// Return the integer version of a roman numeral string
+
+// Test Cases: 
+// Test 1
+s1 = "III"
+// 3
+
+// Test 2
+s2 = "LVIII"
+// 58
+
+// Test 3
+s = "MCMXCIV"
+// 1994
+
+
+// Function
+symbols = {
+    I : 1,
+    V : 5,
+    X : 10,
+    L : 50,
+    C : 100,
+    D : 500,
+    M : 1000
+}
+
+var romanToInt = function(s) {
+    for(let x = 0; x < s.length; x++) {
+        
+    }
+};
+
+
+// Testing
+console.log(romanToInt(s1))
