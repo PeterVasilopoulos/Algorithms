38ab1530299afbb99e16af4c133440fc641edad8
PeterVasilopoulos <peterjvasilopoulos@gmail.com>
2023-04-26 11:49:04 -0400

    Algo completed

--- 2-Python_Algos/Apr26.js
+++ 2-Python_Algos/Apr26.js
@@ -11,6 +11,9 @@ const expected1 = "cba";
 const str2 = "";
 const expected2 = "";
 
+const str3 = "Testing a string";
+const expected3 = "gnirts a gnitseT";
+
 /**
  * Recursively reverses a string.
  * - Time: O(?).
@@ -19,7 +22,18 @@ const expected2 = "";
  * @returns {string} The given str reversed.
  */
 function reverseStr(str) {
-    
+    if(str.length == 0) {
+        return "";
+    }
+
+    let endIndex = str.slice(str.length - 1, str.length);
+    let rest = str.slice(0, str.length - 1);
+
+    return endIndex + reverseStr(rest);
 }
 
 /*****************************************************************************/
+
+console.log(reverseStr(str1))
+console.log(reverseStr(str2))
+console.log(reverseStr(str3))
