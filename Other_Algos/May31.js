360e7a3196a0496eeef0ff869694658470bf15e8
PeterVasilopoulos <peterjvasilopoulos@gmail.com>
2023-05-31 11:14:19 -0400

    Algo created

---
+++ Other_Algos/May31.js
@@ -0,0 +1,19 @@
+// Climbing stairs
+// you can either step up 1 or 2 stairs at a time, 
+// how many possible combinations are there to get to the top?
+
+
+// Test Cases:
+const input1 = 2
+const expected1 = 2
+
+const input2 = 3
+const expected2 = 3
+
+const input3 = 10
+
+
+//Function 
+const climbStairs = function(n) {
+
+}
