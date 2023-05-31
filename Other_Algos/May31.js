670ded21c1262d2263f498e6fe1976504f90d70a
PeterVasilopoulos <peterjvasilopoulos@gmail.com>
2023-05-31 12:10:48 -0400

    Algo completed

--- Other_Algos/May31.js
+++ Other_Algos/May31.js
@@ -10,10 +10,40 @@ const expected1 = 2
 const input2 = 3
 const expected2 = 3
 
-const input3 = 10
+const input3 = 4
 
 
 //Function 
-const climbStairs = function(n) {
+var climbStairs = function (n) {
+    if (n == 1) {
+        return 1
+    }
 
-}
+    const sequence = []
+
+    for (let i = 0; i <= n; i++) {
+        if (i == 0 || i == 1) {
+            sequence.push(1)
+        } else {
+            nextNum = sequence[i - 1] + sequence[i - 2]
+            sequence.push(nextNum)
+        }
+    }
+
+    return sequence[sequence.length - 1]
+};
+
+console.log(climbStairs(input1))
+console.log(climbStairs(input2))
+console.log(climbStairs(input3))
+// console.log(climbStairs(45))
+
+// let mem = []
+// var climbStairs2 = function (n) {
+//     if (n <= 2) return n;
+//     if (mem[n] != undefined) return mem[n]
+//     mem[n] = climbStairs2(n - 1) + climbStairs2(n - 2)
+//     return mem[n]
+// };
+
+// console.log(climbStairs2(4))
